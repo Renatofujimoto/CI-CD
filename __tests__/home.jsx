b7c829f7787a4b50cbde85d7f1da550529788ff8
Renatofujimoto <renatofujimoto2@gmail.com>
2024-01-04 16:07:55 -0300

    fix: change javascript for typescript

--- __tests__/home.jsx
+++ __tests__/home.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../app/page";
+import "@testing-library/jest-dom";
 
 describe("Home component", () => {
   test("renders the main section", () => {
