d108ecde4960eeb3947d101a8c25132027df2a53
Renatofujimoto <renatofujimoto2@gmail.com>
2024-01-04 16:00:26 -0300

    first commit

---
+++ __tests__/home.jsx
@@ -0,0 +1,58 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
+
+describe("Home component", () => {
+  test("renders the main section", () => {
+    render(<Home />);
+    const mainElement = screen.getByRole("main");
+    expect(mainElement).toBeInTheDocument();
+  });
+
+  test('renders the "Get started by editing" text', () => {
+    render(<Home />);
+    const textElement = screen.getByText(/Get started by editing/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("renders the code file path", () => {
+    render(<Home />);
+    const codeElement = screen.getByText(/app\/page.tsx/i);
+    expect(codeElement).toBeInTheDocument();
+  });
+
+  test("renders the Vercel logo", () => {
+    render(<Home />);
+    const logoElement = screen.getByAltText(/Vercel Logo/i);
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  test("renders the Next.js logo", () => {
+    render(<Home />);
+    const logoElement = screen.getByAltText(/Next.js Logo/i);
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  test('renders the "Docs" link', () => {
+    render(<Home />);
+    const linkElement = screen.getByRole("link", { name: /Docs/i });
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('renders the "Learn" link', () => {
+    render(<Home />);
+    const linkElement = screen.getByRole("link", { name: /Learn/i });
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('renders the "Templates" link', () => {
+    render(<Home />);
+    const linkElement = screen.getByRole("link", { name: /Templates/i });
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('renders the "Deploy" link', () => {
+    render(<Home />);
+    const linkElement = screen.getByRole("link", { name: /Deploy/i });
+    expect(linkElement).toBeInTheDocument();
+  });
+});
